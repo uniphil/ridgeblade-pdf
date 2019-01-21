36b90ffe9916428523202adcbfe74f916ab52b32
phil <uniphil@gmail.com>
2019-01-20 21:15:56 -0500

    add host configuration

--- app.js
+++ app.js
@@ -3,14 +3,17 @@
 const puppeteer = require('puppeteer');
 const Koa = require('koa');
 const info = (...m) => console.log(...m);
+const env = (k, d) => process.env[k] || ( process.env.PRODUCTION
+  ? (console.error(`ERROR: missing env key '${k}'`), process.exit(1))
+  : d);
 
-const reporter = (browser, secret) => async ctx => {
+const reporter = (browser, pat, secret) => async ctx => {
   ctx.assert(ctx.path.startsWith('/report'), 404, `bad path: '${ctx.path}'`);
   ctx.assert(ctx.query.id, 400, `bad query: ${JSON.stringify(ctx.query)}`);
   ctx.assert(ctx.query.secret === secret, 403, `nope`);
   const {id, hash} = ctx.query;
   const page = await browser.newPage();
-  const response = await page.goto(`http://localhost:8000/project/${id}/report/for-pdf?hash=${hash}&secret=${secret}`, {
+  const response = await page.goto(`${pat}/project/${id}/report/for-pdf?secret=${secret}`, {
     waitUntil: 'networkidle2',
   });
 
@@ -32,13 +35,9 @@ const reporter = (browser, secret) => async ctx => {
   info(`successfully fetched and rendered report ${id}`);
 };
 
-
 async function bootstrap() {
-  const secret = process.env['PDF_SECRET'];
-  if (!secret) {
-    console.error('missing PDF_SECRET in env');
-    process.exit(1);
-  }
+  const secret = env('PDF_SECRET', 'secret');
+  const pat = env('PAT_HOST', 'http://localhost:8000');
 
   info('starting browser...');
 
@@ -53,7 +52,7 @@ async function bootstrap() {
   info('launching koa app...');
 
   new Koa()
-    .use(reporter(browser, secret))
+    .use(reporter(browser, pat, secret))
     .listen(3000);
 
   info('flying.');
